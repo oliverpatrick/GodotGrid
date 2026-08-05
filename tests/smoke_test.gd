976bab2f2037d3b10cf22507078ae58590d6869a
oli <opholroyd@gmail.com>
2026-08-05 22:26:06 +0100

    fix: launch main scene without editor cache

--- tests/smoke_test.gd
+++ tests/smoke_test.gd
@@ -2,6 +2,9 @@ extends RefCounted
 
 
 static func run() -> bool:
+	var configured_main_scene := str(ProjectSettings.get_setting("application/run/main_scene"))
+	if configured_main_scene.is_empty() or load(configured_main_scene) == null:
+		return false
 	var scene: PackedScene = load("res://terrain_generator.tscn")
 	if ProjectSettings.get_setting("application/config/name") != "GodotGrid" or scene == null:
 		return false
