f4caa3f120e21fb37bd27cb3dc3481bb0e93415f
oli <opholroyd@gmail.com>
2026-08-06 14:35:39 +0100

    feat: encode inspect and use requests

--- tests/protocol_test.gd
+++ tests/protocol_test.gd
@@ -15,6 +15,15 @@ static func run() -> bool:
 	var move: PackedByteArray = Protocol.encode_move(80, 81, 0, 1, 7)
 	if move.hex_encode() != vectors[0].hex:
 		return false
+	var requests := [
+		[Protocol.encode_inspect_world(0x8000002a), "001300048000002a"],
+		[Protocol.encode_inspect_inventory(3), "0014000103"],
+		[Protocol.encode_use_inventory(3, 4), "001500020304"],
+		[Protocol.encode_use_world(3, 0x8000002a, 9), "00160009038000002a00000009"],
+	]
+	for request in requests:
+		if request[0].hex_encode() != request[1]:
+			return false
 	var spawn_bytes: PackedByteArray = str(vectors[5].hex).hex_decode()
 	var spawn_frame = Protocol.decode_frame(spawn_bytes)
 	var spawn = Protocol.decode_message(spawn_frame.id, spawn_frame.payload)
